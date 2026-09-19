1e9057ed56efc5264a49e94d9a4b6d052ca52549
dsk73 <dhananjayskokate@gmail.com>
2026-09-19 15:28:39 +0530

    Updated promotion content

--- src/components/sections/promotion/PromotionContent.tsx
+++ src/components/sections/promotion/PromotionContent.tsx
@@ -4,6 +4,7 @@
 
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 interface PromotionContentProps {
   content: string;
@@ -16,125 +17,220 @@ export default function PromotionContent({ content }: PromotionContentProps) {
 
   return (
     <article className="w-full">
-      <div
-        className="
-          text-base
-          leading-8
-          text-white/70
-          sm:text-lg
-          sm:leading-8
+      <div className="w-full overflow-x-auto">
+        <div
+          className="
+            text-base
+            leading-8
+            text-white/70
+            sm:text-lg
+            sm:leading-8
 
-          [&>h1]:mb-8
-          [&>h1]:text-3xl
-          [&>h1]:font-black
-          [&>h1]:tracking-tight
-          [&>h1]:text-white
-          sm:[&>h1]:text-4xl
+            /* =================================================
+               HEADINGS
+            ================================================= */
 
-          [&>h2]:mb-5
-          [&>h2]:mt-14
-          [&>h2]:text-2xl
-          [&>h2]:font-bold
-          [&>h2]:tracking-tight
-          [&>h2]:text-white
-          sm:[&>h2]:text-3xl
+            [&>h1]:mb-8
+            [&>h1]:mt-0
+            [&>h1]:text-3xl
+            [&>h1]:font-black
+            [&>h1]:leading-tight
+            [&>h1]:tracking-tight
+            [&>h1]:text-white
+            sm:[&>h1]:text-4xl
 
-          [&>h3]:mb-4
-          [&>h3]:mt-10
-          [&>h3]:text-xl
-          [&>h3]:font-bold
-          [&>h3]:tracking-tight
-          [&>h3]:text-white
-          sm:[&>h3]:text-2xl
+            [&>h2]:mb-5
+            [&>h2]:mt-16
+            [&>h2]:text-2xl
+            [&>h2]:font-bold
+            [&>h2]:leading-tight
+            [&>h2]:tracking-tight
+            [&>h2]:text-white
+            sm:[&>h2]:mt-20
+            sm:[&>h2]:text-3xl
 
-          [&>p]:mb-6
-          [&>p]:text-white/70
+            [&>h3]:mb-4
+            [&>h3]:mt-12
+            [&>h3]:text-xl
+            [&>h3]:font-bold
+            [&>h3]:leading-tight
+            [&>h3]:tracking-tight
+            [&>h3]:text-white
+            sm:[&>h3]:text-2xl
 
-          [&>ul]:mb-7
-          [&>ul]:ml-6
-          [&>ul]:list-disc
-          [&>ul]:space-y-3
+            /* =================================================
+               PARAGRAPHS
+            ================================================= */
 
-          [&>ol]:mb-7
-          [&>ol]:ml-6
-          [&>ol]:list-decimal
-          [&>ol]:space-y-3
+            [&>p]:mb-5
+            [&>p]:text-white/70
+            [&>p:last-child]:mb-0
 
-          [&>li]:pl-1
-          [&>li]:text-white/70
+            /* =================================================
+               LISTS
+            ================================================= */
 
-          [&_strong]:font-bold
-          [&_strong]:text-white
+            [&>ul]:mb-8
+            [&>ul]:ml-6
+            [&>ul]:list-disc
+            [&>ul]:space-y-3
 
-          [&_em]:italic
-          [&_em]:text-white/80
+            [&>ol]:mb-8
+            [&>ol]:ml-6
+            [&>ol]:list-decimal
+            [&>ol]:space-y-3
 
-          [&_a]:font-medium
-          [&_a]:text-[#ff1764]
-          [&_a]:underline
-          [&_a]:underline-offset-4
-          [&_a]:transition-colors
-          [&_a]:duration-200
-          [&_a:hover]:text-white
+            [&>li]:pl-1
+            [&>li]:text-white/70
 
-          [&>blockquote]:my-8
-          [&>blockquote]:rounded-xl
-          [&>blockquote]:border-l-4
-          [&>blockquote]:border-[#ff1764]
-          [&>blockquote]:bg-[#ff1764]/5
-          [&>blockquote]:px-6
-          [&>blockquote]:py-5
-          [&>blockquote]:text-white/75
+            /* =================================================
+               STRONG / EMPHASIS
+            ================================================= */
 
-          [&>hr]:my-12
-          [&>hr]:border-white/10
+            [&_strong]:font-bold
+            [&_strong]:text-white
 
-          [&>pre]:my-8
-          [&>pre]:overflow-x-auto
-          [&>pre]:rounded-xl
-          [&>pre]:border
-          [&>pre]:border-white/10
-          [&>pre]:bg-black/40
-          [&>pre]:p-5
+            [&_em]:italic
+            [&_em]:text-white/80
 
-          [&_code]:rounded
-          [&_code]:bg-white/5
-          [&_code]:px-1.5
-          [&_code]:py-0.5
-          [&_code]:text-sm
-          [&_code]:text-white/90
+            /* =================================================
+               LINKS
+            ================================================= */
 
-          [&>pre_code]:bg-transparent
-          [&>pre_code]:p-0
+            [&_a]:font-medium
+            [&_a]:text-[#ff1764]
+            [&_a]:underline
+            [&_a]:underline-offset-4
+            [&_a]:transition-colors
+            [&_a]:duration-200
+            [&_a:hover]:text-white
 
-          [&>table]:my-8
-          [&>table]:w-full
-          [&>table]:overflow-hidden
-          [&>table]:rounded-xl
-          [&>table]:border
-          [&>table]:border-white/10
+            /* =================================================
+               BLOCKQUOTE
+            ================================================= */
 
-          [&_th]:border
-          [&_th]:border-white/10
-          [&_th]:bg-white/5
-          [&_th]:px-4
-          [&_th]:py-3
-          [&_th]:text-left
-          [&_th]:font-semibold
-          [&_th]:text-white
+            [&>blockquote]:my-10
+            [&>blockquote]:rounded-xl
+            [&>blockquote]:border-l-4
+            [&>blockquote]:border-[#ff1764]
+            [&>blockquote]:bg-[#ff1764]/5
+            [&>blockquote]:px-6
+            [&>blockquote]:py-5
+            [&>blockquote]:text-white/75
 
-          [&_td]:border
-          [&_td]:border-white/10
-          [&_td]:px-4
-          [&_td]:py-3
-          [&_td]:text-white/70
+            /* =================================================
+               HORIZONTAL RULE
+               Hidden intentionally
+            ================================================= */
 
-          [&>img]:my-8
-          [&>img]:max-w-full
-          [&>img]:rounded-xl
-        "
-      >
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+            [&>hr]:my-8
+            [&>hr]:h-0
+            [&>hr]:border-0
+            [&>hr]:bg-transparent
+
+            /* =================================================
+               CODE BLOCKS
+            ================================================= */
+
+            [&>pre]:my-8
+            [&>pre]:overflow-x-auto
+            [&>pre]:rounded-xl
+            [&>pre]:border
+            [&>pre]:border-white/10
+            [&>pre]:bg-black/40
+            [&>pre]:p-5
+
+            [&_code]:rounded
+            [&_code]:bg-white/5
+            [&_code]:px-1.5
+            [&_code]:py-0.5
+            [&_code]:text-sm
+            [&_code]:text-white/90
+
+            [&>pre_code]:bg-transparent
+            [&>pre_code]:p-0
+
+            /* =================================================
+               TABLE CONTAINER
+            ================================================= */
+
+            [&>table]:my-10
+            [&>table]:w-full
+            [&>table]:min-w-190
+            [&>table]:border-separate
+            [&>table]:border-spacing-x-0
+            [&>table]:border-spacing-y-3
+
+            /* =================================================
+               TABLE HEADER
+            ================================================= */
+
+            [&_th]:border-0
+            [&_th]:bg-white/5
+            [&_th]:px-4
+            [&_th]:py-4
+            [&_th]:text-left
+            [&_th]:font-bold
+            [&_th]:text-white
+
+            [&_th:first-child]:rounded-l-xl
+            [&_th:last-child]:rounded-r-xl
+
+            /* =================================================
+               TABLE ROWS
+            ================================================= */
+
+            [&_tbody_tr]:bg-white/2.5
+            [&_tbody_tr]:transition-colors
+            [&_tbody_tr:hover]:bg-white/4.5
+
+            /* =================================================
+               TABLE CELLS
+            ================================================= */
+
+            [&_td]:border-0
+            [&_td]:px-4
+            [&_td]:py-4
+            [&_td]:align-middle
+            [&_td]:text-white/70
+
+            [&_tbody_td:first-child]:rounded-l-xl
+            [&_tbody_td:last-child]:rounded-r-xl
+
+            /* =================================================
+               TABLE CELL LINE BREAKS
+            ================================================= */
+
+            [&_td_br]:block
+            [&_td_br]:h-2
+            [&_td_br]:content-['']
+
+            /* =================================================
+               IMAGES
+            ================================================= */
+
+            [&>img]:my-8
+            [&>img]:max-w-full
+            [&>img]:rounded-xl
+
+            /* =================================================
+               MOBILE TABLE
+            ================================================= */
+
+            sm:[&_th]:px-5
+            sm:[&_th]:py-4
+
+            sm:[&_td]:px-5
+            sm:[&_td]:py-4
+          "
+        >
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw]}
+          >
+            {content}
+          </ReactMarkdown>
+        </div>
       </div>
     </article>
   );
